08e7ec0f0382e53e7524d7901ddbf16dc6901657
JacoDTDev <jacodtdev@gmail.com>
2019-09-20 16:04:13 +0200

    116. Exploring Mocha and Expect grouping the test cases

--- imports/api/user.test.js
+++ imports/api/user.test.js
@@ -4,19 +4,32 @@ const add = (a,b)=>{
     }
     return a+b;
 };
-//test for the first return
-it('should add two numbers',function(){
-    const res = add(11,9);
 
-    if (res!==20){
-        throw new Error('Sum was not equal to expected value')
-    }
-});
+//create a group
+describe('add',function () {
+    //test for the first return
+    it('should add two numbers',function(){
+        const res = add(11,9);
+
+        if (res!==20){
+            throw new Error('Sum was not equal to expected value')
+        }
+    });
 //test for the second return
-it('should double a single number',function () {
-    const res =add(44);
+    it('should double a single number',function () {
+        const res =add(44);
 
-    if(res!==88){
-        throw new Error('Number was not doubled')
+        if(res!==88){
+            throw new Error('Number was not doubled')
+        }
+    });
+});
+
+//next example
+const square = (a)=>a*a;
+it('should square a number',function () {
+    const res = square(11);
+    if(res !==121){
+        throw new Error('Did not square number');
     }
 });
