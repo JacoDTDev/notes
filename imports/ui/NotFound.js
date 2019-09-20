1872d60a0c925cd3d4fafbe4c20585324796670e
JacoDTDev <jacodtdev@gmail.com>
2019-09-20 14:11:00 +0200

    init commit

---
+++ imports/ui/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from "react-router";
+
+export default ()=>{
+    return <div className='boxed-view'>
+        <div className='boxed-view__box'>
+            <h1>404= Page Not Found</h1>
+            <p>We`re unable to find that page.</p>
+            <Link to="/" className='button button--link'>Head Home</Link>
+        </div>
+    </div>
+}
