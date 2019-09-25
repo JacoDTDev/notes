b4bc460cb28888dfe9ac5f91a36871a3d3be18c6
JacoDTDev <jacodtdev@gmail.com>
2019-09-25 09:41:57 +0200

    117.2 Using An Assertion Library using the exact code

--- imports/api/user.test.js
+++ imports/api/user.test.js
@@ -11,15 +11,17 @@ describe('add',function () {
     //test for the first return
     it('should add two numbers',function(){
         const res = add(11,9);
-
-        if (res!==20){
-            throw new Error('Sum was not equal to expected value')
-        }
+        //using expect
+        expect(res).toBe(20);
+        //instead of
+        // if (res!==20){
+        //     throw new Error('Sum was not equal to expected value')
+        // }
     });
 //test for the second return
     it('should double a single number',function () {
         const res =add(44);
-
+        expect(res).toBe(88);
         if(res!==88){
             throw new Error('Number was not doubled')
         }
@@ -32,9 +34,10 @@ const square = (a)=>a*a;
 describe('square',function () {
     it('should square a number',function () {
         const res = square(11);
-        if(res !==121){
-            throw new Error('Did not square number');
-        }
+        expect(res).toBe(121);
+        // if(res !==121){
+        //     throw new Error('Did not square number');
+        // }
     });
 });
 
